24a0885765d84e0e7911b0fcc987b459496fe3ca
LavaRT <pavlo.legominecraft@gmail.com>
2025-04-11 11:54:43 +0300

    Добавьте файлы проекта.

---
+++ gRCP-Server/Services/MessageServiceImpl.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using GrpcMessageCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gRCP_Server.Services
+{
+    public class MessageServiceImpl : MessageService.MessageServiceBase
+    {
+        public override async Task StreamMessages(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            int count = 0;
+        }
+    }
+}
